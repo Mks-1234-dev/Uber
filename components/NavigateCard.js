610bb78aed157bf7c6135c8f0731d39dd5684845
Mks-1234-dev <seemasatya2@gmail.com>
2024-07-28 22:56:02 -0500

    16

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -18,6 +18,32 @@ const NavigateCard = () => {
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning Jesse</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyles}
+            placeholder="Where to?"
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+
+              navigation.navigate("RideOptionsCard");
+            }}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
         <NavFavourites />
       </View>
 
